dc97706a0c780f9dfa376623fd8e81bd0da500f9
James Sharma <65190976+JamesSharma04@users.noreply.github.com>
2023-03-30 15:38:17 +0100

    updated directory for averages file

--- src/home.py
+++ src/home.py
@@ -54,7 +54,7 @@ if upload_runs is not None:
     except Exception as e:
         st.sidebar.exception(f"Failed. Error {e}")
 if upload_runs is None:
-    df = pd.read_csv('../data/processed/scout/average_utils/averages.csv')
+    df = pd.read_csv('averages.csv')
     st.session_state['df'] = df
     original_df = df.copy()
     with st.spinner("Training model"):
